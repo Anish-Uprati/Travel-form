110f9595cbcd317be9163a2e12987510968c9e71
Anish Upreti <uanish767@gmail.com>
2026-02-10 21:44:16 +0545

    i put the php and html in same page.

--- index.php
+++ index.php
@@ -1,4 +1,51 @@
 
+<?php
+$insert = false;
+if (isset($_POST['name'])){
+    //set connection variables.
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    
+    //Create a database connection.
+    $con = mysqli_connect($server, $username, $password);
+
+    //Check for connection success.
+    if(!$con){
+        die("connection to this database failed due to" .
+            mysqli_connect_error());
+    }
+    // echo "Success connecting to the db";
+    
+    //collect post variables.
+    $name = $_POST['name'];
+    $gender = $_POST['gender'];
+    $age = $_POST['age'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $decs = $_POST['decs'];
+    
+    $sql = "INSERT INTO `trip`.`trip` (`name`, `age`, `gender`, `email`, `phone`, `decs`, `dt`) VALUES ('$name',
+            '$age', '$gender', '$email', '$phone', '$decs',current_timestamp());";
+    // echo $sql;
+    
+    //Execute the query.
+    if ($con->query($sql) == true) {
+        //  echo"Successfully inserted";
+
+        //Flag for successful inserctuion.
+        $insert= true;
+    }
+    else{
+        echo "ERROR: $sql <br> $con->error";
+    }
+
+    //close the database connection.
+    $con->close();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,8 +62,13 @@
         <h1>Welcome to College Pokhara Trip form.</h1>
         <p>Enter your details and submit this form to confirm 
            your participation in the trip.</p>
-        <p class="msg">Thanks for submitting your form.
-           We are happy to see you joining us for Pokhara trip.</p>
+    <?php  
+        if ($insert == true) {
+        echo "<p class='msg'>Thanks for submitting your form.
+           We are happy to see you joining us for Pokhara trip.</p>";
+        }
+    ?>
+        
    <form action="index.php" method="post">
       <input type="text" name="name" id="name" placeholder="Enter your name">
         <input type="text" name="age" id="age" placeholder="Enter your Age">
